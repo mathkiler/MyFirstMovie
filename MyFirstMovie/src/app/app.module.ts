adc3f6c9b8535ee6250b2408b36b440f6af33f2f
Mathieu-ab <mathieu3577aversenc@gmail.com>
2023-03-17 12:11:47 +0100

    ajout authetification
    ajout des pages sign-up et sign-in (pas encore fonctionnel)

--- MyFirstMovie/src/app/app.module.ts
+++ MyFirstMovie/src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 
+import { AuthService } from "./shared/services/auth.service";
+import { AngularFireModule, FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from "../environements/environment";
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService, {provide : FIREBASE_OPTIONS, useValue: environment.firebase}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ MyFirstMovie/src/environements/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyBYgqjz_MtHwTBmIsScl46U0oS-nDuFelY",
+      authDomain: "myfirstmovie-14716.firebaseapp.com",
+      projectId: "myfirstmovie-14716",
+      storageBucket: "myfirstmovie-14716.appspot.com",
+      messagingSenderId: "259980805201",
+      appId: "1:259980805201:web:08c29d35f218501e076470"
+    
+    }
+  };
